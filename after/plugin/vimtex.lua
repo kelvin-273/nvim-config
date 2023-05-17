7ad5781b633e5c20bd0c048dc26d347be143f803
Kelvin Davis <273degreeskelvin@gmail.com>
2023-05-17 17:34:35 +1000

    OS-specific viewer for vimtex

---
+++ after/plugin/vimtex.lua
@@ -0,0 +1,12 @@
+-- Determine the host system
+local is_mac = vim.fn.has("mac") == 1
+local is_linux = vim.fn.has("unix") == 1 and not is_mac
+
+-- Define viewers based on OS
+if is_mac then
+  vim.g.vimtex_view_method = 'skim'
+elseif is_linux then
+  print("going down this path")
+  vim.g.vimtex_view_method = 'zathura'
+else
+end
