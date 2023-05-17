43b58fc11af1d2c1a369c42184c4448981e1ad0c
Kelvin Davis <273degreeskelvin@gmail.com>
2023-05-17 22:48:15 +1000

    cleaning

--- after/plugin/vimwiki.lua
+++ after/plugin/vimwiki.lua
@@ -1,7 +1,7 @@
 -- TODO: Get this to create the menu with both options <23-04-23> --
 vim.g.vimwiki_list = {
   {},
-  { 
+  {
     name = 'phd',
     path = '~/Documents/Research/PhD/Kelvin-s-PhD-wiki',
     path_html = '~/Documents/Research/PhD/Kelvin-s-PhD-wiki/html',
