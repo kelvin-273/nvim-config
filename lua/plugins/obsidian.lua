364cda8424337d9178d6a8543689a662f4031877
Kelvin Davis <kelvin.davis1@monash.edu>
2025-11-17 14:47:35 +1100

    Lazy load obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,5 +1,6 @@
 return {
   "obsidian-nvim/obsidian.nvim",
+  lazy = true,
   version = "*", -- recommended, use latest release instead of latest commit
   --ft = "markdown",
   cmd = { "Obsidian" },
